f744e3672fce727601483bf32d42d8502f4f0d59
Carvas <76731842+lCarvas@users.noreply.github.com>
2022-11-23 22:42:33 +0000

    Pre Release 1

--- sheets.py
+++ sheets.py
@@ -1,24 +1,26 @@
 from __future__ import print_function
-import datetime
-
-today = datetime.datetime.now().strftime('%d/%m/%Y')
-today = datetime.datetime.strptime(today,'%d/%m/%Y')
-
-import os.path
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+import datetime
+from pathlib import Path
+import os
+
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '1SdZGuAU3WBXOrxiPzJaFSoSTatwcYFy6tkEvsbU6uAE'
-SAMPLE_RANGE_NAME = 'Form responses 1!B2:L'
-DATE_RANGE_NAME = 'MAIN!A2:A'
+SAMPLE_RANGE_NAME = 'Main!A2:L'
+
+maindir = os.path.join(Path.home(), 'Desktop\\AutoMM\\')
+today = datetime.datetime.now().strftime('%d/%m/%Y')
+today = datetime.datetime.strptime(today,'%d/%m/%Y')
+
 
 
 def main():
@@ -44,7 +46,7 @@ def main():
             token.write(creds.to_json())
 
     try:
-        service = build('sheets', 'v4', credentials=creds)
+        service = build('sheets', 'v4', credentials=creds, static_discovery=False)
 
         # Call the Sheets API
         sheet = service.spreadsheets()
@@ -54,22 +56,55 @@ def main():
         if not values:
             print('No data found.')
             return
-
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                    range=DATE_RANGE_NAME).execute()
-        dates = result.get('values', [])
-        if not dates:
-            print('No data found.')
-            return
-
-
-        for row in values:
-            if today <= datetime.datetime.strptime(row[0], '%d/%m/%Y '):
-                if row[1] == 'Novo Hinário':
-                    fnh = open('novo hinário.txt','x')
-                    fnh.write(f'{row[1]}\n 1º{row[2]} 2º{row[3]}')
-                    print(row)
         
+        os.makedirs(os.path.dirname(maindir), exist_ok=True)
+
+        # Index Variables
+        nhi = 0
+        ci = 0
+        esi = 0
+        mei = 0
+
+        # Start the bat file
+        batfile = open(maindir + 'Open Me.bat','w')
+        batfile.write('@echo off\n')
+
+        for row in reversed(values):
+            if today <= datetime.datetime.strptime(row[0], '%d/%m/%Y'):
+                #TODO Create a txt file with the info necessary, 
+                if row[1] == 'Novo Hinário':
+                    if nhi == 0:
+                        batfile.write(f'::Novo Hinário\nstart {row[2]}\nstart {row[3]}\n\n')
+                        nhi += 1
+                    else:
+                        pass
+
+                elif row[1] == 'Culto':
+                    if ci == 0:
+                        batfile.write(f'::Culto\nstart {row[2]}\nstart {row[3]}\n\n')
+                        ci += 1
+                    else: 
+                        pass
+                
+                #TODO Adicionar auto video carta missionaria at some point
+                elif row[1] == 'Escola Sabatina':
+                    if esi == 0:
+                        batfile.write(f'::Escola Sabatina\nstart {row[2]}\nstart {row[3]}\n\n')
+                        esi += 1
+                    else:
+                        pass
+                
+                elif row[1] == 'Momento Especial':
+                    if mei == 0:
+                        if row[11] == 'Não':
+                            checkstart = '::Sem Música'
+                        else:
+                            checkstart = f'start {row[11]}'
+                        batfile.write(f'::Momento Especial\n::{row[10]}\n{checkstart}\n\n')
+                        mei += 1
+                    else:
+                        pass
+
 
     except HttpError as err:
         print(err)
