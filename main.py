ea4448b893e37dcf3f20ae20abf34756a1cabaca
Carvas <76731842+lCarvas@users.noreply.github.com>
2023-01-17 01:20:37 +0000

    NV -> MDL, + AN + PDT, ME update, + linkver function
    - ME now supports files
    - hymndic made global
    - fixed a lot of stuff in file_writing
    - linkver function to accomodate for numbers in hymn cells

--- main.py
+++ main.py
@@ -72,9 +72,11 @@ def main():
         os.makedirs(os.path.dirname(maindir), exist_ok=True)
 
         dic = {
-            'Novo Hinário':['NV',0],
+            'Momentos de Louvor':['MDL',0],
             'Culto':['C',0],
             'Escola Sabatina':['ES',0],
+            'Anúncios':['AN',0],
+            'Programa da Tarde':['PDT',0],
             'Momento Especial':['ME',0,{
                 'Durante a Escola Sabatina':0,
                 'Após a Escola Sabatina':0,
@@ -97,7 +99,7 @@ def main():
             if today <= datetime.datetime.strptime(row[0], '%d/%m/%Y'):
                 if row[1] == 'Momento Especial':
                     if dic['Momento Especial'][1] == 0:
-                        batfile.write(f'start https://www.google.com/search?q=ME')
+                        batfile.write(f'start https://www.google.com/search?q=ME\n')
                         txtfile.write(f'{row[1]}')
                         dic['Momento Especial'][1] += 1
                     for time in dic['Momento Especial'][2].keys():
@@ -106,6 +108,10 @@ def main():
                                 if row[11] != 'Não':
                                     batfile.write(f'\nstart {row[11]}\n')
                                 txtfile.write(f'\n{row[10]}\n{number_get(row[11])}\n\n')
+                                if row[4] != '':
+                                    txtfile.write(f'{necfiles(row[4],maindir,creds)}\n\n')
+                                else:
+                                    txtfile.write('\n')
                                 dic['Momento Especial'][2][row[10]] += 1
                             else:
                                 pass
@@ -118,4 +124,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-input('\nFinished.')
+input('Finished.')
