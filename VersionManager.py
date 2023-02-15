542036d4342b4b23c9b85e8c85ba1c464944a6b9
Carvas <76731842+lCarvas@users.noreply.github.com>
2023-02-15 12:45:52 +0000

    Testing auto update

--- VersionManager.py
+++ VersionManager.py
@@ -1,6 +1,7 @@
 # got from https://raw.githubusercontent.com/LeagueOfPoro/CapsuleFarmerEvolved/master/src/VersionManager.py 
 
 import requests as req
+from main import maindir
 
 class VersionManager:
 
@@ -15,3 +16,20 @@ class VersionManager:
     @staticmethod
     def isLatestVersion(currentVersion):
         return currentVersion >= VersionManager.getLatestTag()
+
+def download_file(url, filename=''):
+    try:
+        if filename:
+            pass            
+        else:
+            filename = req.url[downloadUrl.rfind('/')+1:]
+
+        with req.get(url) as req:
+            with open(filename, 'wb') as f:
+                for chunk in req.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+            return filename
+    except Exception as e:
+        print(e)
+        return None
