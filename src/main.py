814c4407e258a95fe25077caa69825a2b47095e5
Carvas <76731842+lCarvas@users.noreply.github.com>
2023-02-16 23:01:05 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -44,18 +44,18 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists(os.path.abspath('config/token.json')):
+        creds = Credentials.from_authorized_user_file(os.path.abspath('config/token.json'), SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                resource_path(os.path.join(os.pardir,'credentials\\credentials.json')), SCOPES)
+                resource_path('../config/credentials.json'), SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open(os.path.abspath('config/token.json'), 'w') as token:
             token.write(creds.to_json())
 
     try:
