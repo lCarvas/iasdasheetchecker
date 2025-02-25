ad5b87a6ac460fc6bc6405c9d7df2c86811eb705
Carvas <76731842+lCarvas@users.noreply.github.com>
2025-02-25 12:43:20 +0000

    .

--- src/config.py
+++ src/config.py
@@ -12,50 +12,45 @@ class Config:
             config = yaml.safe_load(f)
 
         if setting == "sheetid":
-            return config.get("ids")["spreadsheetid"]
+            return config.get("spreadsheetid")
 
         if setting == "youtube":
-            return config.get("settings")["youtube"]
+            return config.get("youtube")
 
     @staticmethod
     def createConfig() -> None:
         with open("./config/config.yaml", "w") as f:
             print("Creating config file..")
 
-            returnDict: dict[str, str | bool] = {"ids": {}, "settings": {}}
+            returnDict: dict[str, str | bool] = {}
 
-            returnDict["ids"]["spreadsheetid"] = input(
-                "Please input the Spreadsheet ID: "
-            )
+            returnDict["spreadsheetid"] = input("Please input the Spreadsheet ID: ")
 
             while (
                 youtubeUsage := input("Do you want to use YouTube? (Y/N): ").upper()
             ) not in ["Y", "N"]:
                 pass
 
-            returnDict["settings"]["youtube"] = True if youtubeUsage == "Y" else False
+            returnDict["youtubeUsage"] = True if youtubeUsage == "Y" else False
 
             yaml.safe_dump(returnDict, f)
 
         print("Config file created.")
-        print("Please place the credentials file inside the config folder.")
-        input(
-            "Press Enter after you put the credentials file inside the config folder."
-        )
+        input("Press Enter to continue.")
 
     @staticmethod
     def swapBool() -> None:
         with open(path.abspath("config/config.yaml"), "r", encoding="utf-8") as f:
             config = yaml.safe_load(f)
 
-        if config.get("settings")["youtube"]:
-            config["settings"]["youtube"] = False
+        if config.get["youtubeUsage"]:
+            config["youtubeUsage"] = False
 
             with open(path.abspath("config/config.yaml"), "w", encoding="utf-8") as f:
                 config = yaml.safe_dump(config, f, default_flow_style=False)
             return
 
-        config["settings"]["youtube"] = True
+        config["youtubeUsage"] = True
         with open(path.abspath("config/config.yaml"), "w", encoding="utf-8") as f:
             config = yaml.safe_dump(config, f, default_flow_style=False)
         return
@@ -65,10 +60,10 @@ class Config:
         with open(path.abspath("config/config.yaml"), "r", encoding="utf-8") as f:
             config = yaml.safe_load(f)
 
-        config["ids"]["spreadsheetid"] = input(
+        config["spreadsheetid"] = input(
             "Please input the Spreadsheet ID (leave empty to discard changes): "
         )
-        if config["ids"]["spreadsheetid"] == "":
+        if config["spreadsheetid"] == "":
             return
         with open(path.abspath("config/config.yaml"), "w", encoding="utf-8") as f:
             config = yaml.safe_dump(config, f, default_flow_style=False)
@@ -76,10 +71,15 @@ class Config:
     @staticmethod
     def verifyConfig():
         try:
-            with open("./config/config.yaml", "r") as f:
+            with open(path.abspath("config/config.yaml"), "r") as f:
                 config = yaml.safe_load(f)
-                config.get("ids").get("spreadsheetid")
-                config.get("settings").get("youtube")
+
+                for key in [
+                    "spreadsheetid",
+                    "youtubeusage",
+                ]:
+                    if config.get(key) is None:
+                        raise AttributeError
 
         except AttributeError or FileNotFoundError:
             print("Config file invalid or not found, creating...")
